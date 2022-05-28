36b1db6e0a772dce67c0a29890c4542fcc43d022
MOSTA3D <khabbieny123@gmail.com>
2022-05-28 15:10:20 +0200

    editing earliest deadline first algorithm

--- Assignment3/Mlf/src/Main.java
+++ Assignment3/Mlf/src/Main.java
@@ -29,37 +29,62 @@ public class Main {
         int hp;
         int temp;
         while(true){
-            hp = 0;
-            temp = tasks[0][0] * (runningTasks[0][0] + 1) - currentTime - tasks[0][1] + runningTasks[0][1];
+            // detecting if the current time is less than the beginning of any period in the tasks
+            int taskArrivalTime = tasks[0][0] * runningTasks[0][0];
             for(int i = 1; i < nTasks; i++){
-                // equation = deadline - currentTime - remainingExecTime
-                int temp2 = tasks[i][0] * (runningTasks[i][0] + 1) - currentTime - tasks[i][1] + runningTasks[i][1];
-                if(temp > temp2){
-                    temp = temp2;
-                    hp = i;
+                if(taskArrivalTime > runningTasks[i][0] * tasks[i][1]){
+                    taskArrivalTime = runningTasks[i][0] * tasks[i][1];
                 }
             }
 
-            System.out.println("task " + hp + " is now running");
-
-            int timeToFinish = (currentTime + tasks[hp][1] - runningTasks[hp][1]);
-            int timeBreak = timeToFinish;
-
-            for(int i = 1; i < nTasks; i++){
-                int minTaskEnter = (runningTasks[i][0] + 1) * tasks[i][0];
-                if(timeBreak > minTaskEnter){
-                    timeBreak = minTaskEnter;
-                }
-            }
-
-            if(timeBreak == timeToFinish){
-                runningTasks[hp][0]++;
-                runningTasks[hp][1] = 0;
+            if(taskArrivalTime > currentTime){
+                System.out.println("No tasks running at time " + currentTime);
+                currentTime = taskArrivalTime;
             }else{
-                runningTasks[hp][1] += (currentTime - timeBreak);
-            }
+                hp = 0;
+                temp = 0;
+                for(int i = 0; i < nTasks; i++){
+                    if((tasks[i][0] * runningTasks[i][0] <= currentTime)){
+                        temp = tasks[i][0] * (runningTasks[i][0] + 1) - tasks[i][1] + runningTasks[i][1];
+                        hp = i;
+                        break;
+                    }
+                }
 
-            currentTime += timeBreak;
+
+                for(int i = 1; i < nTasks - 1; i++){
+                    // equation = deadline - currentTime - remainingExecTime
+                    if((tasks[i][0] * runningTasks[i][0] <= currentTime)){
+                        int temp2 = tasks[i][0] * (runningTasks[i][0] + 1) - tasks[i][1] + runningTasks[i][1];
+                        if(temp > temp2){
+                            temp = temp2;
+                            hp = i;
+                        }
+                    }
+                }
+
+                System.out.println("task J(" + hp + ',' + runningTasks[hp][0] + ") is now running at time " + currentTime);
+
+                int timeToFinish = (currentTime + tasks[hp][1] - runningTasks[hp][1]);
+                int timeBreak = timeToFinish;
+
+                for(int i = 1; i < nTasks; i++){
+                    int minTaskEnter = (runningTasks[i][0] + 1) * tasks[i][0];
+                    if(timeBreak > minTaskEnter){
+                        timeBreak = minTaskEnter;
+                    }
+                }
+
+                if(timeBreak == timeToFinish){
+                    runningTasks[hp][0]++;
+                    runningTasks[hp][1] = 0;
+                }else{
+                    runningTasks[hp][1] += (currentTime - timeBreak);
+                }
+
+                currentTime += timeBreak;
+
+            }
 
             try {
                 Thread.sleep(500);
